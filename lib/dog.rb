bd67e8047ba0a618dffa028a6affbbaeb1f2ba6f
liuh <hanke.liu@gmail.com>
2018-10-30 12:07:17 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,10 @@
+class Dog
+
+  def bark
+    puts "Woff!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+
+  def talk
+      puts "Hello World"
+  end
+
+  def walk
+    puts "The Persona is walking"
+  end
+end
